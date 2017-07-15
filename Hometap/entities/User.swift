bfd01e5800834a07df0c2a749d354df322ccc5b9
Daniel Soto <dansotorey@gmail.com>
2017-07-15 14:29:10 -0500

    User flie fix

--- Hometap/entities/User.swift
+++ Hometap/entities/User.swift
@@ -7,13 +7,13 @@
 //
 
 import Foundation
+import Firebase
 
-class User: NSObject {
+class User: HometapObject {
     
-    public init(dict: [String: AnyObject]){
-        if let uid = dict["id"] {
-            self.uid = (uid as? String)
-        }
+    public override init(dict: [String: AnyObject]){
+        super.init(dict: dict)
+        
         if let name = dict["name"] {
             self.name = (name as? String)
         }
@@ -46,7 +46,41 @@ class User: NSObject {
         }
     }
     
-    var uid: String?
+    override func save(route: String) {
+        if self.name != nil {
+            original_dictionary["name"] = self.name as AnyObject
+        }
+        if self.birth != nil {
+            original_dictionary["birth"] = self.birth!.toString(format: .Default) as AnyObject
+        }
+        if self.joined != nil {
+            original_dictionary["joined"] = self.joined!.toString(format: .Long) as AnyObject
+        }
+        if self.gender != nil {
+            original_dictionary["gender"] = self.gender as AnyObject
+        }
+        if self.provider != nil {
+            original_dictionary["provider"] = self.provider as AnyObject
+        }
+        if self.photo != nil {
+            original_dictionary["photo"] = self.photo as AnyObject
+        }
+        if self.email != nil {
+            original_dictionary["email"] = self.email as AnyObject
+        }
+        if self.rating != nil {
+            original_dictionary["rating"] = self.rating as AnyObject
+        }
+        if self.phone != nil {
+            original_dictionary["phone"] = self.phone as AnyObject
+        }
+        if self.votes != nil {
+            original_dictionary["votes"] = self.votes as AnyObject
+        }
+        
+        super.save(route: route)
+    }
+    
     var name: String?
     var birth: Date?
     var joined: Date?
@@ -57,5 +91,65 @@ class User: NSObject {
     var rating: Double?
     var phone: String?
     var votes: Int?
+    
+    public func services_brief() -> [Service]? {
+        var services_brief:[Service] = []
+        if let srvc = original_dictionary["upcomingServices"] {
+            if let srvcDict = srvc as? [String:AnyObject] {
+                for (_, service) in srvcDict {
+                    if let servDict = service as? [String:AnyObject] {
+                        services_brief.append(Service(dict: servDict))
+                    }
+                }
+                return services_brief
+            }
+        }
+        return nil
+    }
+    
+    public func services() -> [Service]? {
+        var services_brief:[Service] = []
+        if let srvc = original_dictionary["upcomingServices"] {
+            if let srvcDict = srvc as? [String:AnyObject] {
+                for (id_service, _) in srvcDict {
+                    Service.withID(id: id_service, callback: {(service) in
+                        services_brief.append(service)
+                    })
+                }
+                return services_brief
+            }
+        }
+        return nil
+    }
+    
+    public func history_brief() -> [Service]? {
+        var history_brief:[Service] = []
+        if let srvc = original_dictionary["pastServices"] {
+            if let srvcDict = srvc as? [String:AnyObject] {
+                for (_, service) in srvcDict {
+                    if let servDict = service as? [String:AnyObject] {
+                        history_brief.append(Service(dict: servDict))
+                    }
+                }
+                return history_brief
+            }
+        }
+        return nil
+    }
+    
+    public func history() -> [Service]? {
+        var history:[Service] = []
+        if let srvc = original_dictionary["pastServices"] {
+            if let srvcDict = srvc as? [String:AnyObject] {
+                for (id_service, _) in srvcDict {
+                    Service.withID(id: id_service, callback: {(service) in
+                        history.append(service)
+                    })
+                }
+                return history
+            }
+        }
+        return nil
+    }
     
 }
